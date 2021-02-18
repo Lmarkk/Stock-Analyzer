84f898de28eac53fec54d3c6a18cc9f695b1dd38
Sorellum <lassi.markkinen@hotmail.com>
2021-02-18 16:59:29 +0200

    Import components in module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppUpwardTrendComponent } from './app-upward-trend/app-upward-trend.component';
+import { AppVolumePriceChangeComponent } from './app-volume-price-change/app-volume-price-change.component';
+import { AppOpenPriceSmaComponent } from './app-open-price-sma/app-open-price-sma.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppUpwardTrendComponent,
+    AppVolumePriceChangeComponent,
+    AppOpenPriceSmaComponent,
   ],
   imports: [
     BrowserModule,
